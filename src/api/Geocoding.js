5674bff2fd7da9df7e4c07aca1d0a11322a6be91
Sheep <hank3681@gmail.com>
2022-04-06 17:16:56 +0800

    add: 封裝取得地理座標 api 函式

---
+++ src/api/Geocoding.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+const key = `${process.env.VUE_APP_KEY}`
+const baseURL = 'http://api.openweathermap.org/geo/1.0/direct'
+
+/**
+ * 取得城市的經緯度
+ * @param {String} city - 城市名稱
+ * @returns Promise
+ */
+export const getCityLocation = (city) => {
+  return request(`${baseURL}?q=${city}&limit=2&appid=${key}`, 'get')
+}
