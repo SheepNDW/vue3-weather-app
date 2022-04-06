509045e9baacd51ac336af9bd55b418a9e8e77cb
Sheep <hank3681@gmail.com>
2022-04-06 20:51:53 +0800

    update: 設定預設城市以及讀取切換

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
-  <div class="container">
+  <div class="container" v-show="!isLoading">
     <!-- weather(left) -->
     <WeatherSide />
     <!-- info(right) -->
     <InfoSide />
   </div>
+  <LoadingPage />
 </template>
 
 <script>
 import WeatherSide from '@/components/WeatherSide.vue'
 import InfoSide from '@/components/InfoSide.vue'
+import LoadingPage from '@/components/LoadingPage.vue'
+import { useStatusStore } from './stores/status'
+import { useWeatherStore } from './stores/weather'
+import { computed } from 'vue'
 
 export default {
   name: 'App',
   components: {
     WeatherSide,
-    InfoSide
+    InfoSide,
+    LoadingPage
+  },
+  setup() {
+    const statusStore = useStatusStore()
+
+    const weatherStore = useWeatherStore()
+    weatherStore.getWeather()
+
+    return {
+      isLoading: computed(() => statusStore.isLoading)
+    }
   }
 }
 </script>
