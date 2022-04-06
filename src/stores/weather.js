aed9663c2ea22f786ca33aa903da7d744e4f1268
Sheep <hank3681@gmail.com>
2022-04-06 20:54:11 +0800

    update: 完成 stores 之間資料傳遞&請求錯誤截取

--- src/stores/weather.js
+++ src/stores/weather.js
@@ -1,14 +1,15 @@
 import { defineStore } from 'pinia'
 import { getCityLocation } from '@/api/Geocoding'
 import { getWeatherData } from '@/api/WeatherData'
+import { useStatusStore } from './status'
 
 export const useWeatherStore = defineStore('weatherStore', {
   state: () => {
     return {
       coordinates: {
-        city: '',
-        lat: '',
-        lon: ''
+        city: '臺北市',
+        lat: '25.0375198',
+        lon: '121.5636796'
       },
       current: {
         dt: '',
@@ -35,17 +36,30 @@ export const useWeatherStore = defineStore('weatherStore', {
   },
   actions: {
     async searchCity(cityName) {
-      const data = await getCityLocation(cityName)
-      this.coordinates.lat = data[0].lat
-      this.coordinates.lon = data[0].lon
-      this.coordinates.city = data[0].local_names.zh
-      this.getWeather()
+      const useStatus = useStatusStore()
+      useStatus.isLoading = true
+      try {
+        const data = await getCityLocation(cityName)
+        this.coordinates.lat = data[0].lat
+        this.coordinates.lon = data[0].lon
+        this.coordinates.city = data[0].local_names.zh
+        this.getWeather()
+      } catch (error) {
+        useStatus.errMsg = '無效的城市名 請重新輸入! '
+        setTimeout(() => {
+          useStatus.isLoading = false
+          useStatus.errMsg = ''
+        }, 2500)
+      }
     },
     async getWeather() {
       const data = await getWeatherData(this.coordinates)
       const { dt, temp, weather } = data.current
       this.current = { dt, temp, weather }
       this.daily = data.daily
+
+      const useStatus = useStatusStore()
+      useStatus.isLoading = false
     }
   }
 })
